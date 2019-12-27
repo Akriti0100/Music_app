69fd581d5fe7ee51366de0e3ccfccbb5020e889a
Akriti0100 <akritisinghal1663@gmail.com>
2019-12-28 00:54:33 +0530

    django_project added

---
+++ music/urls.py
@@ -0,0 +1,22 @@
+
+from django.urls import path
+from . import views
+
+
+app_name = 'music'
+
+
+urlpatterns = [
+    # /music/
+    path('', views.IndexView.as_view() , name='index'),
+
+    #/music/71/
+    path('<int:pk>/', views.DetailView.as_view() , name='detail' ),
+
+    #/music/album/add/
+    path('album/add/' , views.AlbumCreate.as_view() ,name='album-add'),
+
+    #/music/71/favourite/
+    #path('<int:album_id>/favourite/', views.favourite , name='favourite' ),
+]
+
