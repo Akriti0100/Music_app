8c946f2a81d29bf828dc8163db064cbf135e29e6
Akriti0100 <akritisinghal1663@gmail.com>
2019-12-28 22:16:22 +0530

    model_forms created

--- music/urls.py
+++ music/urls.py
@@ -16,6 +16,12 @@ urlpatterns = [
     #/music/album/add/
     path('album/add/' , views.AlbumCreate.as_view() ,name='album-add'),
 
+    #/music/album/2/
+    path('album/<int:pk>/' , views.AlbumUpdate.as_view() ,name='album-update'),
+
+    #/music/album/2/delete/
+    path('album/<int:pk>/delete/' , views.AlbumDelete.as_view() ,name='album-delete'),
+
     #/music/71/favourite/
     #path('<int:album_id>/favourite/', views.favourite , name='favourite' ),
 ]
